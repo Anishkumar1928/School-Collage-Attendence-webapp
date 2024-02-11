fea56f8a8893410dff7bc446fe654f20aad1503b
Anish kumar <112718941+Anishkumar1928@users.noreply.github.com>
2024-02-12 00:36:00 +0530

    Update loginsys.py

--- loginsys.py
+++ loginsys.py
@@ -1,17 +1,6 @@
 import openpyxl
 from openpyxl import workbook
 
-# def loginu(userid,password):
-#     wb = openpyxl.load_workbook('data base.xlsx')
-#     sheet = wb['Sheet1']
-#     id_=[]    
-#     for row in range(2,sheet.max_row+1):
-#         user_value=str(sheet.cell(row=row, column=1).value)
-#         pass_value=str(sheet.cell(row=row, column=2).value)
-#         name_value
-#         if user_value==userid and password==pass_value:
-#             return True
-
 
 def loginu(userid,password):
     wb = openpyxl.load_workbook('data base.xlsx')
